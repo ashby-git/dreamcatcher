7d4d6b88f51bcb865271537536a1136be10af340
Ashby McFall <ashbymcfall@hotmail.co.uk>
2022-09-30 09:37:17 +0900

    🚧Add dummy-data to homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,86 +1,60 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+import { GetServerSideProps, GetStaticProps } from "next";
+import Head from "next/head";
+import DreamList from "../components/dreams/DreamList";
+import { IDreamItem } from "../types/types";
 
-const Home: NextPage = () => {
-  return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
-
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and its API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
-      </footer>
-    </div>
-  )
+interface HomePageProps {
+  dreams: [IDreamItem];
 }
 
-export default Home
+const DUMMY_DREAMS = [
+  {
+    id: "d1",
+    title: "First Dream",
+    image:
+      "https://images.unsplash.com/photo-1563902452959-f07de6bf4d7f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2340&q=80",
+    location: "first city, first country",
+    description: "This is the first dream on dreamcatcher.",
+  },
+  {
+    id: "d2",
+    title: "Second Dream",
+    image:
+      "https://images.unsplash.com/photo-1563902452959-f07de6bf4d7f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2340&q=80",
+    location: "second city, second country",
+    description: "This is the second dream on dreamcatcher.",
+  },
+];
+
+const HomePage = ({ dreams }: HomePageProps) => {
+  return (
+    <div className="">
+      <Head>
+        <title>Dreamcatcher</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <DreamList dreams={dreams} />
+    </div>
+  );
+};
+
+// export const getServerSideProps: GetServerSideProps = async (context) => {
+//   const req = context.req;
+//   const res = context.res;
+//   return {
+//     props: {
+//       dreams: DUMMY_DREAMS,
+//     },
+//   };
+// };
+
+export const getStaticProps: GetStaticProps = async () => {
+  return {
+    props: {
+      dreams: DUMMY_DREAMS,
+    },
+    revalidate: 3,
+  };
+};
+
+export default HomePage;
