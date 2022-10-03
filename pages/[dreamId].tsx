3d75aed4decf8cd6130f7c7baa69bcbdb14e56ec
Ashby McFall <ashbymcfall@hotmail.co.uk>
2022-10-03 11:44:23 +0900

    🚧Add DreamDetailsPage w/ dummyData

---
+++ pages/[dreamId].tsx
@@ -0,0 +1,14 @@
+import DreamDetail from "../components/dreams/DreamDetail";
+
+function DreamDetailsPage() {
+  return (
+    <DreamDetail
+      image="https://images.unsplash.com/photo-1563902452959-f07de6bf4d7f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2340&q=80"
+      title="First Dream"
+      location="Some City, Some Country"
+      description="This is an example of a dream. I dreamed that I was flying in the sky. It was so beautiful. I felt weightless and could fly in any direction that I wanted to. I flew over forests, in and out of buildings, anything was possible."
+    />
+  );
+}
+
+export default DreamDetailsPage;
