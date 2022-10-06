a0ac058ef1069f55c9a33f71f7dd797591366c1d
Ashby McFall <ashbymcfall@hotmail.co.uk>
2022-10-06 21:23:41 +0900

    ✨fetch-POST data from NewDreamPage

--- pages/new-dream.tsx
+++ pages/new-dream.tsx
@@ -1,9 +1,22 @@
+import { useRouter } from "next/router";
 import NewDreamForm from "../components/dreams/NewDreamForm";
 import { IDreamItem } from "../types/types";
 
 function NewDreamPage() {
-  function addDreamHandler(dreamData: IDreamItem) {
-    console.log(dreamData);
+  const router = useRouter();
+
+  async function addDreamHandler(enteredDreamData: IDreamItem) {
+    const response = await fetch("/api/new-dream", {
+      method: "POST",
+      body: JSON.stringify(enteredDreamData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await response.json();
+    console.log(data);
+
+    router.push("/");
   }
   return (
     <div>
